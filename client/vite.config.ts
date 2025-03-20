df8f3e35cf74e2ccb9fbd2451723eab44c0cc337
yasho11 <147644615+yasho11@users.noreply.github.com>
2025-03-20 16:04:59 +0545

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,4 +4,8 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+    server: {
+    host: '0.0.0.0',  // Bind to all network interfaces
+    port: 4173        // Or any port that Render will bind to
+  },
 });
